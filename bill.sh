fdd0b1fcbcebc05bb82dbe34058a81c7d33545da
Mike Lindner <mikelindner@mail.com>
2025-12-09 17:35:00 +1030

    changed to mikes.page

--- bill.sh
+++ bill.sh
@@ -5,7 +5,7 @@ echo -n "Please enter URL: " && read urlink
 echo -n "Enter Title: " && read destitle
 
 # now get the list of available short links
-aws s3 cp --quiet s3://mikes.link/randoms /tmp
+aws s3 cp --quiet s3://mikes.page/randoms /tmp
 
 # pop the top short link into linknumber
 linknumber=$(head -1 /tmp/randoms)
@@ -21,7 +21,7 @@ echo "
         <meta charset=\"utf-8\">
         <title>$destitle</title>
         <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-        <link rel=\"stylesheet\" href=\"../mikes.link.css\">
+        <link rel=\"stylesheet\" href=\"../mikes.page.css\">
         <meta property="og:image" content="../star.png" />
         <script type = \"text/javascript\">
             window.location = \"$urlink\";
@@ -41,7 +41,7 @@ echo "
 aws s3 cp --quiet /tmp/index.html s3://mikes.link/$linknumber/
 
 # put the list of available links back up
-aws s3 cp --quiet /tmp/randoms s3://mikes.link/
+aws s3 cp --quiet /tmp/randoms s3://mikes.page/
 
 # clean up
 rm /tmp/index.html
